cb72bf7dce8d32e05383a3ea20d0f9d4cee9c58e
Himanshu <himanshu.chowdhary.iitkgp@gmail.com>
2016-10-04 15:47:45 +0530

    data generator & line chart ready
    data generation function and create line chart function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,20 @@
-var socket = io.connect('http://localhost:3000');
-var result = 0;
-socket.on('add', function (data) {
-	result += parseInt(data.value);
-	// console.log(result);
-	$('#add').text(data.value);
-	$('#result').text(result);
-});
-socket.on('sub', function (data) {
-	result -= parseInt(data.value);
-	// console.log(result);
-	$('#subtract').text(data.value);
-	$('#result').text(result);
-});
+var dataset = dataGenerator(200);
+createLineChart(dataset);
+
+
+
+
+// var socket = io.connect('http://localhost:3000');
+// var result = 0;
+// socket.on('add', function (data) {
+// 	result += parseInt(data.value);
+// 	// console.log(result);
+// 	$('#add').text(data.value);
+// 	$('#result').text(result);
+// });
+// socket.on('sub', function (data) {
+// 	result -= parseInt(data.value);
+// 	// console.log(result);
+// 	$('#subtract').text(data.value);
+// 	$('#result').text(result);
+// });
--- public/javascripts/visualization.js
+++ public/javascripts/visualization.js
@@ -1,9 +1,56 @@
-var margin = {
-	top: 30,
-	right: 20,
-	bottom: 30,
-	left: 20
+var dataGenerator = function(n) {
+	var arr = [];
+	for (var i = 0; i < n; i++) {
+		arr.push({
+			"index": i,
+			"value": d3.randomNormal(20,2.5)()
+		});
+	}
+	return arr;
 };
 
-var width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+
+function createLineChart(data) {
+	var margin = {top: 20, right: 20, bottom: 30, left: 50},
+			width = 600 - margin.left - margin.right,
+			height = 300 - margin.top - margin.bottom;
+
+	var x = d3.scaleLinear().range([0, width]);
+	var y = d3.scaleLinear().range([height, 0]);
+
+	var line = d3.line()
+	    .x(function(d) { return x(d.index); })
+	    .y(function(d) { return y(d.value); });
+
+	var svg = d3.select("#graphs").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform",
+	          "translate(" + margin.left + "," + margin.top + ")");
+
+	x.domain(d3.extent(data, function(d) { return d.index; }));
+	y.domain(d3.extent(data, function(d) { return d.value; }));
+
+	svg.append("g")
+	    .attr("class", "axis axis--x")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(d3.axisBottom(x));
+
+	svg.append("g")
+	    .attr("class", "axis axis--y")
+	    .call(d3.axisLeft(y))
+	  .append("text")
+	    .attr("class", "axis-title")
+	    .attr("transform", "rotate(-90)")
+	    .attr("y", 6)
+	    .attr("dy", ".71em")
+	    .style("text-anchor", "end")
+	    .text("Price ($)");
+
+	svg.append("path")
+	    .datum(data)
+	    .attr("class", "line")
+	    .attr("d", line);
+}
+
