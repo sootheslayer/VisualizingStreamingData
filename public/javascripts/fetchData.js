3b7b6237cd6c7e2947e3c465b6fabfe44a2a06d5
Himanshu <himanshu.chowdhary.iitkgp@gmail.com>
2016-10-19 16:34:02 +0530

    Scatter chart of user summary working with queries.

--- public/javascripts/fetchData.js
+++ public/javascripts/fetchData.js
@@ -1,40 +1,103 @@
 // Initialize websocket connection
 var socket = io.connect('http://localhost:3000');
 
-var bouncePages;
-var bouncePagesData = [
+// var bouncePages;
+// var bouncePagesData = [
+// {
+// 	key: "Top 10 BR Pages",
+// 	values: []
+// }];
+
+// nv.addGraph(function() {
+//     bouncePages = nv.models.multiBarHorizontalChart()
+//         .x(function(d) { return d.label })
+//         .y(function(d) { return d.value })
+//         .margin({top: 20, right: 20, bottom: 20, left: 1})
+//         .showControls(false)        //Allow user to switch between "Grouped" and "Stacked" mode.
+//         .barColor(d3.scale.category20().range());
+
+//     bouncePages.yAxis
+//         .tickFormat(d3.format(',.2f'));
+
+//     d3.select('#randomTest')
+//     	.append('svg')
+//         .datum(bouncePagesData)
+//         .transition().duration(500)
+//         .call(bouncePages);
+
+//     nv.utils.windowResize(bouncePages.update);
+
+//     return bouncePages;
+// });
+
+var shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'];
+var userSummaryChart;
+var userSummaryChartData = [
 {
-	key: "Top 10 BR Pages",
+	key: "Condo",
 	values: []
-}];
+},
+{
+	key: "vehicle",
+	values: []
+},
+{
+	key: "Home Owner",
+	values: []
+},
+{
+	key: "Umbrella",
+	values: []
+},
+{
+	key: "Renters",
+	values: []
+},
+{
+	key: "Flood",
+	values: []
+}
+];
 
 nv.addGraph(function() {
-    bouncePages = nv.models.multiBarHorizontalChart()
-        .x(function(d) { return d.label })
-        .y(function(d) { return d.value })
-        .margin({top: 20, right: 20, bottom: 20, left: 1})
-        .showControls(false)        //Allow user to switch between "Grouped" and "Stacked" mode.
-        .barColor(d3.scale.category20().range());
+    userSummaryChart = nv.models.scatterChart()
+        .showDistX(true)
+        .showDistY(true)
+        .showLabels(true)
+        .useVoronoi(true)
+        .color(d3.scale.category10().range())
+        .duration(300)
+        .clipEdge(true)
+    ;
+    userSummaryChart.dispatch.on('renderEnd', function(){
+        console.log('render complete');
+    });
 
-    bouncePages.yAxis
-        .tickFormat(d3.format(',.2f'));
+    userSummaryChart.xAxis
+    			.axisLabel('Time Spent (in mins)')
+    			.tickFormat(d3.format('.02f'));
+    
+    userSummaryChart.yAxis
+    			.axisLabel('Total hits')
+					.tickFormat(d3.format('.02f'));
 
     d3.select('#randomTest')
-    	.append('svg')
-        .datum(bouncePagesData)
-        .transition().duration(500)
-        .call(bouncePages);
+    		.append('svg')
+        .datum(userSummaryChartData)
+        .transition().duration(1000)
+        .call(userSummaryChart);
 
-    nv.utils.windowResize(bouncePages.update);
+    nv.utils.windowResize(userSummaryChart.update);
 
-    return bouncePages;
+    userSummaryChart.dispatch.on('stateChange', function(e) { ('New State:', JSON.stringify(e)); });
+    return userSummaryChart;
 });
 
-
 $(document).ready(function(){
+
 	// Pull data out cassandra by button
 	$("#refreshData").click(function(){
-		var query = "SELECT * FROM pages limit 10";
+		var query = "SELECT * FROM test_purchase_summary limit 50;";
 		console.log('sending query to server: ' + query);
 		socket.emit('fetch-data', query);
 	});
@@ -42,17 +105,26 @@ $(document).ready(function(){
 }); // Document ready function closing bracket
 
 socket.on('fetched-result',function(data){
-	bouncePagesData[0].values = [];
+	// bouncePagesData[0].values = [];
+	userSummaryChartData.forEach(function(elem){elem.values = [];}); // Remove old values from chart
 	data.forEach(function(row){
-		// console.log(row);
-		bouncePagesData[0].values.push({label: row.page, value: row.time});
+		var index = userSummaryChartData.findIndex(function(elem){
+			if(elem.key === row.insurance){
+			return true;
+			}
+		})
+		userSummaryChartData[index].values.push({
+			x: row.time_spent,
+			y: row.visits_count,
+			size: row.age,
+			shape: shapes[index]
+		});
 	});
-	bouncePages.update();
-	console.log("updated chart");
+	 userSummaryChart.update();
 });
 
-setInterval(function(){
-	var query = "SELECT * FROM pages LIMIT 10";
-	socket.emit('fetch-data', query);
-	console.log("query sent to cassandra: " + query);
-}, 2000);
+// setInterval(function(){
+// 	var query = "SELECT * FROM pages LIMIT 10";
+// 	socket.emit('fetch-data', query);
+// 	console.log("query sent to cassandra: " + query);
+// }, 2000);
