426425e39bf0923a9726a047f5a92173b07159bb
Himanshu <himanshu.chowdhary.iitkgp@gmail.com>
2016-10-21 12:35:55 +0530

    Google Maps added and working

--- public/javascripts/fetchData.js
+++ public/javascripts/fetchData.js
@@ -3,6 +3,7 @@ var socket = io.connect('http://localhost:3000');
 
 //-------------------------------------- CHART DEFINITIONS --------------------------------------
 
+// Conversion Summary Small Chart
 var shapes = ['circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'];
 var conversionScatterChart;
 var conversionScatterChartData = [
@@ -31,7 +32,6 @@ var conversionScatterChartData = [
 	values: []
 }
 ];
-
 nv.addGraph(function() {
     conversionScatterChart = nv.models.scatterChart()
         .margin({right: 40})
@@ -67,6 +67,8 @@ nv.addGraph(function() {
     return conversionScatterChart;
 });
 
+
+// Conversion Summary Modal Chart
 var conversionScatterModalChart;
 nv.addGraph(function() {
     conversionScatterModalChart = nv.models.scatterChart()
@@ -102,6 +104,49 @@ nv.addGraph(function() {
     conversionScatterModalChart.dispatch.on('stateChange', function(e) { ('New State:', JSON.stringify(e)); });
     return conversionScatterModalChart;
 });
+
+
+// User distribution on map
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('userMapChart'), {
+    zoom: 3,
+    center: {lat: -28.024, lng: 140.887},
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var locations = [
+    {lat: -31.563910, lng: 147.154312},
+    {lat: -33.718234, lng: 150.363181},
+    {lat: -33.727111, lng: 150.371124},
+    {lat: -33.848588, lng: 151.209834},
+    {lat: -33.851702, lng: 151.216968},
+    {lat: -34.671264, lng: 150.863657},
+    {lat: -35.304724, lng: 148.662905},
+    {lat: -36.817685, lng: 175.699196},
+    {lat: -36.828611, lng: 175.790222},
+    {lat: -37.750000, lng: 145.116667},
+    {lat: -37.759859, lng: 145.128708},
+    {lat: -37.765015, lng: 145.133858},
+    {lat: -37.770104, lng: 145.143299},
+    {lat: -37.773700, lng: 145.145187},
+    {lat: -37.774785, lng: 145.137978},
+    {lat: -37.819616, lng: 144.968119},
+    {lat: -38.330766, lng: 144.695692},
+    {lat: -39.927193, lng: 175.053218},
+    {lat: -41.330162, lng: 174.865694},
+    {lat: -42.734358, lng: 147.439506},
+    {lat: -42.734358, lng: 147.501315},
+    {lat: -42.735258, lng: 147.438000},
+    {lat: -43.999792, lng: 170.463352}
+  ]
+  var markers = locations.map(function(location, i) {
+    return new google.maps.Marker({
+      position: location,
+    });
+  });
+  var markerCluster = new MarkerClusterer(map, markers, {imagePath: '../images/m'});
+}
+
 
 //------------------------------------- jQuery Definitions -----------------------------------
 
